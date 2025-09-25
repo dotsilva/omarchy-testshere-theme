2b734d272b584981077502648cd7086298fa16ed
dotsilva <devbrunodasilva@gmail.com>
2025-09-24 23:50:17 -0300

    commit

--- neovim.lua
+++ neovim.lua
@@ -1,28 +1,9 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"bjarneo/pixel.nvim",
 		priority = 1000,
 		config = function()
-			require("catppuccin").setup({
-				flavour = "latte", -- other options: "mocha", "frappe", "macchiato"
-			})
-			vim.cmd.colorscheme("catppuccin-latte")
+			vim.cmd.colorscheme("pixel")
 		end,
-	},
-	{
-    "akinsho/bufferline.nvim",
-    init = function()
-        local bufline = require("catppuccin.groups.integrations.bufferline")
-        function bufline.get()
-          return bufline.get_theme()
-        end
-      end,
-    },
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin-latte",
-		},
 	},
 }
